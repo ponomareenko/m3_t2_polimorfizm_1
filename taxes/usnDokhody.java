c7408f8dd01e513b05d8506915089e205f16592d
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-04-08 18:40:17 +0300

    Update usnDokhody.java

--- taxes/usnDokhody.java
+++ taxes/usnDokhody.java
@@ -3,6 +3,6 @@ package taxes;
 public class usnDokhody extends TaxSystem {
     @Override
     public int calcTaxFor(int debit, int credit) {
-        return 6 / 100 * debit;
+        return (int) (debit * 0.06);
     }
 }
