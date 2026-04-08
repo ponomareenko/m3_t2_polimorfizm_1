ef4b45cbddf7af35e59e232b88228da00282fb66
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-04-08 18:39:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
 import taxes.TaxSystem;
 import taxes.usnDokhody;
+import taxes.usnDokhodyMinusRaskhody;
 
 public class Main {
     static void main(String[] args) {
         TaxSystem taxSystem1 = new usnDokhody();
         Company company1 = new Company("Danfos", taxSystem1);
 
-        company1.debit = 1000;
+        company1.setTaxSystem(new usnDokhodyMinusRaskhody());
+
+        company1.debit = 900;
         company1.credit = 200;
         company1.amount = 100;
-        company1.shiftMoney(company1.amount);
 
-//        System.out.println(company1.debit);
-//        System.out.println(company1.credit);
+        company1.shiftMoney(company1.amount);
 
         company1.payTaxes();
     }
